7078507467dc1fdd8735a6d5651897b8ea661a0c
Martin Mai <mrkanister1024@googlemail.com>
2017-12-24 21:10:53 +0100

    Add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+set -e
+
+DOTFILES_DIR=${DOTFILES_DIR:-~/.config/dotfiles}
+
+echo "dotfiles: bootstrap to $DOTFILES_DIR"
+
+if ! command -v git > /dev/null; then
+    sudo apt-get install -y git
+fi
+
+if [ ! -d $DOTFILES_DIR ]; then
+    echo "dotfiles: clone git repository"
+    git clone -q https://github.com/mrkanister/dotfiles.git $DOTFILES_DIR
+
+    echo "dotfiles: create symlink"
+    sudo ln -s "${DOTFILES_DIR}/dotfiles.sh" /usr/local/bin/dotfiles
+else
+    echo "dotfiles: update git repository"
+    git -C $DOTFILES_DIR pull -q
+fi
