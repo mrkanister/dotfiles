428eeef0dbbeda12f2b8cb976d3c17c0676f3ef8
Martin Mai <mrkanister1024@googlemail.com>
2017-12-24 23:00:45 +0100

    Make dotfiles.sh self updating and quote DOTFILES_DIR

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,18 +7,20 @@ DOTFILES_DIR=${DOTFILES_DIR:-~/.config/dotfiles}
 echo "dotfiles: bootstrap to $DOTFILES_DIR"
 
 if ! command -v git > /dev/null; then
-    sudo apt-get install -y git
+    echo "dotfiles: install git"
+    sudo apt-get install -qq git
 fi
 
-if [ ! -d $DOTFILES_DIR ]; then
+if [ ! -d "$DOTFILES_DIR" ]; then
     echo "dotfiles: clone git repository"
-    git clone -q https://github.com/mrkanister/dotfiles.git $DOTFILES_DIR
-
-    echo "dotfiles: create symlink in /usr/local/bin"
-    sudo ln -s "${DOTFILES_DIR}/dotfiles.sh" /usr/local/bin/dotfiles
+    git clone -q https://github.com/mrkanister/dotfiles.git "$DOTFILES_DIR"
 else
     echo "dotfiles: update git repository"
-    git -C $DOTFILES_DIR pull -q
+    git -C "$DOTFILES_DIR" pull -q
 fi
 
-dotfiles
+TARGET=/usr/local/bin/dotfiles
+echo "dotfiles: create symlink: $TARGET -> $DOTFILES_DIR/dotfiles.sh"
+sudo ln -sf "$DOTFILES_DIR/dotfiles.sh" "$TARGET"
+
+echo "dotfiles: command 'dotfiles' successfully installed"
--- dotfiles.sh
+++ dotfiles.sh
@@ -1,3 +1,12 @@
 #/bin/bash
 
+DOTFILES_DIR="$(dirname "$(readlink -f "$0")")"
+
+# make sure the script itself it update-to-date
+echo "dotfiles: update git repository"
+STATUS=$(git -C "$DOTFILES_DIR" pull)
+if "$STATUS" != "Already up-to-date."; then
+    exec "$0"
+fi
+
 echo "dotfiles: TODO"
