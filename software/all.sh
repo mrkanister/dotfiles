2634bf6b3c8b38818b8b1e54fc14e2819ac36471
Martin Mai <mrkanister1024@googlemail.com>
2017-12-28 21:41:40 +0100

    Filter out packages that are already installed

--- software/all.sh
+++ software/all.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
-xargs sudo apt-get install <<EOF
+SOFTWARE="
+    cloc
+    colordiff
+    curl
+    devscripts
+    git
+    golang-1.9
+    inkscape
+    pwgen
+    tmux
+    tree
+    vim
+"
 
-cloc
-colordiff
-curl
-devscripts
-git
-golang-1.9
-inkscape
-pwgen
-tmux
-tree
-vim
+# Filter packages that are already installed to have cleaner output.
+TO_INSTALL=$(
+    echo $SOFTWARE \
+    | xargs \
+        dpkg-query -W -f='${Status} ${Package}\n' \
+    | grep -v 'install ok installed' \
+    | awk '{print $NF}'
+)
 
-EOF
+if [ -z "$TO_INSTALL" ]; then
+    exit
+fi
+
+echo "dotfiles: install $TO_INSTALL"
+echo $TO_INSTALL | xargs -I{} sudo apt-get -y install {}
