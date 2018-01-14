b328dcf2869382ddeb691049d9e430388252a00b
Martin Mai <mrkanister1024@googlemail.com>
2018-01-14 11:46:16 +0100

    Reformat long alias declaration

--- HOME/.bashrc.d/alias.sh
+++ HOME/.bashrc.d/alias.sh
@@ -1,1 +1,7 @@
-alias susu='sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade && sudo apt-get autoremove && sudo apt-get clean'
+alias susu='\
+    sudo apt-get update \
+ && sudo apt-get upgrade \
+ && sudo apt-get dist-upgrade \
+ && sudo apt-get autoremove \
+ && sudo apt-get clean \
+'
