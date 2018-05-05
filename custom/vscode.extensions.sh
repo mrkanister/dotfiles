069ac149ae122753d6d6d9aeff677afbbaa4c11b
Martin Mai <mrkanister1024@googlemail.com>
2018-05-05 09:00:32 +0200

    vscode: update Go extension ID

--- custom/vscode.extensions.sh
+++ custom/vscode.extensions.sh
@@ -2,7 +2,7 @@
 
 xargs --max-lines=1 code --install-extension > /dev/null <<EOF
     DavidAnson.vscode-markdownlint
-    lukehoban.Go
+    ms-vscode.Go
     stkb.rewrap
     timonwong.shellcheck
 EOF
