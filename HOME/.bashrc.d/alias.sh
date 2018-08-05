97b3840bb3753a12384ef04c8c4ac64aca3ec471
Martin Mai <mrkanister1024@googlemail.com>
2018-08-05 18:17:04 +0200

    Add ls alias 'll' with more natural sorting

--- HOME/.bashrc.d/alias.sh
+++ HOME/.bashrc.d/alias.sh
@@ -4,6 +4,8 @@ alias ..='cd ..'
 
 alias igrep='grep -riI'
 
+alias ll='ls --all --classify --group-directories-first --human-readable -l -X'
+
 alias tree='tree --dirsfirst'
 
 alias wig='tig --ignore-all-space'
