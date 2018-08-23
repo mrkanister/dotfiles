e0ebf7a689d77bf7871bef8f4a4a639d30037903
Martin Mai <mrkanister1024@googlemail.com>
2018-08-24 00:04:02 +0200

    Create $GOPATH/bin to avoid wget error
    When dotfiles is run on a fresh installation, the directory $GOPATH/bin
    doesn't exist yet and causes wget to fail while writing the output file.

--- custom/dep.sh
+++ custom/dep.sh
@@ -4,5 +4,6 @@ if command -v dep > /dev/null; then
     exit
 fi
 
+mkdir -p "$GOPATH/bin"
 wget --quiet --output-document "$GOPATH/bin/dep" \
     "https://github.com/golang/dep/releases/download/v0.5.0/dep-linux-amd64"
