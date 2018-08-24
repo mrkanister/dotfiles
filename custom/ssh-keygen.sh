601b8b2f79c05f8123d9681fb395146c937d3b9a
Martin Mai <mrkanister1024@googlemail.com>
2018-08-24 22:53:37 +0200

    ssh-keygen: pass the default file name 'id_rsa'

--- custom/ssh-keygen.sh
+++ custom/ssh-keygen.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-if [ -f "$HOME/.ssh/id_rsa" ]; then
+KEYFILE="$HOME/.ssh/id_rsa"
+
+if [ -f "$KEYFILE" ]; then
     exit
 fi
 
-ssh-keygen -t rsa -b 4096
+ssh-keygen -t rsa -b 4096 -f "$KEYFILE"
