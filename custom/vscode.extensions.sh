d9cd1364f88a23bcaa49811bbc059787d779483e
Martin Mai <mrkanister1024@googlemail.com>
2018-08-25 13:49:54 +0200

    vscode: install only new extensions
    This saves quite some time that Visual Studio Code would otherwise spend
    on figuring out that the extension to install is already installed.

--- custom/vscode.extensions.sh
+++ custom/vscode.extensions.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-xargs --max-lines=1 code --install-extension > /dev/null <<EOF
-    DavidAnson.vscode-markdownlint
-    ms-vscode.Go
-    stkb.rewrap
-    timonwong.shellcheck
+EXTENSIONS=$(cat <<EOF
+DavidAnson.vscode-markdownlint
+ms-vscode.Go
+stkb.rewrap
+timonwong.shellcheck
 EOF
+)
+
+# Extract lines that are unique to the above extensions list.
+comm -13 <(code --list-extensions) <(cat <<< "$EXTENSIONS") | \
+    while read -r extension; do
+        code --install-extension "$extension"
+    done
