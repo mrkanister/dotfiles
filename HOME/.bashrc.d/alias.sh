aad641e38bc5dc6c0d6735f158837dcc860852ad
Martin Mai <mrkanister1024@googlemail.com>
2021-04-28 21:30:24 +0200

    Update Go to v1.16.3
    Also, remove 'vgo' alias since modules are the default since v1.16.

--- HOME/.bashrc.d/alias.sh
+++ HOME/.bashrc.d/alias.sh
@@ -10,8 +10,6 @@ alias ll='ls --classify --group-directories-first --human-readable -l -X'
 
 alias tree='tree --dirsfirst'
 
-alias vgo='GO111MODULE=on go'
-
 alias wig='tig --ignore-all-space'
 
 alias xclip='xclip -selection clipboard'
