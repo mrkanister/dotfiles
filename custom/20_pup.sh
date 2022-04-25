ca00c66183cffff9a01189a8d941ff0431ea5243
Martin Mai <mrkanister1024@googlemail.com>
2022-04-25 18:31:10 +0200

    software: drop unused and replace youtube-dl with yt-dlp

--- custom/20_pup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if command -v pup > /dev/null; then
-    exit
-fi
-
-go install github.com/ericchiang/pup@latest
--- custom/20_scc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if command -v scc > /dev/null; then
-    exit
-fi
-
-go install github.com/boyter/scc@latest
