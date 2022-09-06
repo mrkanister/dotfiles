b1cddbe7639641c3ca1fc965864c7c02498ef645
Martin Mai <mrkanister1024@googlemail.com>
2022-09-06 18:28:24 +0200

    sounds: remove 'bak' folder first to be sure
    Prevent the prevent the following error in case the folder reappears:
    
      mv: cannot move '/usr/share/sounds' to '/usr/share/sounds_bak/sounds': Directory not empty

--- custom/disable-sounds.sh
+++ custom/disable-sounds.sh
@@ -4,5 +4,6 @@ set -euo pipefail
 SOUNDS_FOLDER="/usr/share/sounds"
 
 if [[ -d ${SOUNDS_FOLDER} ]]; then
+    sudo rm -rf ${SOUNDS_FOLDER}_bak
     sudo mv ${SOUNDS_FOLDER} ${SOUNDS_FOLDER}_bak
 fi
