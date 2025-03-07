2786679a07ff10c70b6624fefd7c3e9f2ad3db58
Martin Mai <mrkanister1024@googlemail.com>
2025-03-07 09:04:44 +0100

    custom: add default apps for more MIME types

--- custom/xdg-mime.sh
+++ custom/xdg-mime.sh
@@ -4,11 +4,15 @@ set -euo pipefail
 ## /var/lib/snapd/desktop/applications
 ## /usr/share/applications
 
+## xdg-mime query filetype <file>
+
+xdg-mime default code.desktop application/x-x509-ca-cert
 xdg-mime default code.desktop text/markdown
 xdg-mime default code.desktop text/plain
 xdg-mime default code.desktop text/tab-separated-values
 xdg-mime default google-chrome.desktop application/pdf
 xdg-mime default org.inkscape.Inkscape.desktop image/svg+xml
 xdg-mime default vlc_vlc.desktop audio/mpeg
+xdg-mime default vlc_vlc.desktop audio/x-opus+ogg
 xdg-mime default vlc_vlc.desktop video/mpeg
 xdg-mime default vlc_vlc.desktop video/webm
