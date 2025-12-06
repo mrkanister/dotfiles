8ad25efeba375d02cd70d7b6bb9c866e1e276d70
Martin Mai <mrkanister1024@googlemail.com>
2025-12-06 11:33:29 +0100

    config: VSCode: update extension list

--- custom/vscode.extensions.sh
+++ custom/vscode.extensions.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 EXTENSIONS=$(cat <<EOF
+dnut.rewrap-revived
 esbenp.prettier-vscode
 golang.go
 jacobcassidy.css-nesting-syntax-highlighting
+jebbs.plantuml
 jock.svg
 mechatroner.rainbow-csv
-stkb.rewrap
 svelte.svelte-vscode
 timonwong.shellcheck
 EOF
 )
 
+INSTALLED=$(code --list-extensions)
+
 # Extract lines that are unique to the above extensions list.
-comm -13 <(code --list-extensions) <(cat <<< "$EXTENSIONS") | \
+comm -13 <(cat <<< "$INSTALLED") <(cat <<< "$EXTENSIONS") | \
     while read -r extension; do
         code --install-extension "$extension"
     done
